b7df2f2f06de7ba1fc89235a2dbdc314050ed002
KiwiDev808 <lucasbacela@hotmail.com>
2021-04-10 15:07:47 -0300

    Course info with modules and functonal methods done

---
+++ part2/course-information/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((acc, part) => acc + part.exercises, 0);
+  return(
+    <p><strong>total of {sum} exercises</strong></p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((item) => <Part key={item.id} part={item} />)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Course
